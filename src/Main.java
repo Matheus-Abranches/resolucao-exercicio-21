d6e50a2faafda791ca45805b56a3534bb5dddef4
xirovinha <xirova.abranches@outlook.com>
2024-07-08 13:22:22 -0300

    resolucao exercicio 20

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random sorteador = new Random();
+
+       int i = 0;
+        while( i <= 0){
+            int valorAleatorio = sorteador.nextInt(100);
+            System.out.println(valorAleatorio);
+            i++;
+        }
+
+
+
+    }
+}
